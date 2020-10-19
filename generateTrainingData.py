b51945856c0bf76bb44f666977288d61a835a0c2
Cooper French <cooperfrench95@gmail.com>
2020-10-19 17:15:44 +0800

    60 percent

--- generateTrainingData.py
+++ generateTrainingData.py
@@ -1,160 +1,69 @@
 import cv2
 import os
 import random
-import numpy as np
 import pickle
-from mtcnn import MTCNN
-import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+###############################
+##################################
+
+
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+print("Num GPUs Available: ", len(physical_devices))
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 IMG_SIZE = 64
-MIN_PICS_PER_PERSON_IN_TRAINING_SET = 50
-TRAIN_DIR = './self-built-masked-face-recognition-dataset/AFDB_face_dataset'
-TEST_DIR = './self-built-masked-face-recognition-dataset/AFDB_masked_face_dataset'
-
-# TRAIN_DIR = './lfw_masked/lfw_train'
-# TEST_DIR = './lfw_masked/lfw_test'
-
-faces = os.listdir(TRAIN_DIR)
-masked = os.listdir(TEST_DIR)
-
-detector = MTCNN()
-
-actualMasked = []
-for maskedPerson in faces:
-    if (len(actualMasked)) > 63:
-        break
-    try:
-        if len(os.listdir(TEST_DIR + '/' + maskedPerson)) > 0 and len(os.listdir(TRAIN_DIR + '/' + maskedPerson)) > 100:
-            actualMasked.append(maskedPerson)
-    except:
-        continue
-ignorelist = []
-for face in faces:
-    for maskedPerson in masked:
-        if face not in actualMasked:
-            ignorelist.append(face)
-
-people_labels = {}
-highest_label = 0
+ImgSourceDirectory = './self-built-masked-face-recognition-dataset/AFDB_face_dataset'
 
 
-def generateData(dir, train=True):
+#ImgSourceDirectory = './Indian-celebritiesBase/Indian-celebrities'
+# https://gist.github.com/SainaGhosh
+# poor results on any model using this dataset - no idea why
 
-    PEOPLE = os.listdir(dir)
-    FEATURES_OUT = 'training_mask_features.pickle'
-    LABELS_OUT = 'training_mask_labels.pickle'
-
-    # These will be used during training validation
-    FEATURES_TEST_OUT = 'testing_mask_features.pickle'
-    LABELS_TEST_OUT = 'testing_mask_labels.pickle'
-
-    # These are the masked images for actual testing of the finished model
-    if not train:
-        FEATURES_OUT = 'validation_mask_features.pickle'
-        LABELS_OUT = 'validation_mask_labels.pickle'
-
-    images = []
-    train_images = []
-
-    # Import images
-    count = -1
-    imageCount = 0
-    for person in PEOPLE:
-        if person in ignorelist:
-            continue
-        else:
-            count += 1
-        path = os.path.join(dir, person)
-        amount = len(os.listdir(path))
-        # Ensure each person has at least 3 images in the validation set
-        amountIgnore = amount - MIN_PICS_PER_PERSON_IN_TRAINING_SET
-        if amountIgnore < MIN_PICS_PER_PERSON_IN_TRAINING_SET and train:
-            continue
-        for imageIndex, image in enumerate(os.listdir(path)):
-            imageCount += 1
-            print('imageCount', imageCount)
-            # Read in images as an array of pixel values
-            image_as_numpy_array = cv2.imread(
-                os.path.join(path, image))
-            # x, y, w, h = cv2.boundingRect(image_as_numpy_array)
-            cropped = None
-            # MTCNN Detects the face
-            detected = detector.detect_faces(image_as_numpy_array)
-            # Convert back to grayscale
-            gray = cv2.cvtColor(image_as_numpy_array, cv2.COLOR_BGR2GRAY)
-            if detected and len(detected) > 0:
-                # These are the face coordinates
-                x, y, w, h = detected[0]['box']
-                print(x, y, w, h)
-                if y < 0:
-                    y = 0
-                if x < 0:
-                    x = 0
-                # Crop to those coordinates
-                cropped = gray[y:y+h, x:x+w]
-            else:
-                cropped = gray
-            # Crop image, removing the bottom half (hopefully the mouth/mask)
-            # plt.imshow(cropped)
-            # plt.show()
-            resized = cv2.resize(cropped, (IMG_SIZE, IMG_SIZE))
-            # cropped = image_as_numpy_array[y:int(y+h / 2), x:x+w]
-            # Transform image to be e.g. 28x28
-            # Index represents the person's name, because apparently we can't pass a string as a classification type
-            if not person in people_labels.keys() and train:
-                people_labels[person] = count
-            elif not person in people_labels.keys():
-                continue
-            if train and imageIndex > amountIgnore:
-                train_images.append([resized, count])
-            elif train and not person in people_labels.keys():
-                images.append([resized, people_labels[person]])
-            else:
-                images.append([resized, people_labels[person]])
-
-    print(people_labels)
-
-    # The images must be shuffled, otherwise the network will learn incorrectly (it will learn to always guess the first guy, then the second guy, etc.)
-    random.shuffle(images)
-    random.shuffle(train_images)
-
-    features = []
-    labels = []
-
-    train_features = []
-    train_labels = []
-
-    for f, l in images:
-        features.append(f)
-        labels.append(l)
-
-    for f, l in train_images:
-        train_features.append(f)
-        train_labels.append(l)
-
-    # Transform features into numpy array
-    # reshape(amount_of_features, dimension, dimension, 1 = grayscale)
-    features = np.array(features).reshape(-1, IMG_SIZE,
-                                          IMG_SIZE, 1).astype('float32') / 255.0
-    labels = np.array(labels).astype('float32')
-
-    # Save for later
-    with open(FEATURES_OUT, 'wb') as f:
-        pickle.dump(features, f)
-
-    with open(LABELS_OUT, 'wb') as l:
-        pickle.dump(labels, l)
-
-    if train:
-        train_features = np.array(train_features).reshape(-1, IMG_SIZE,
-                                                          IMG_SIZE, 1).astype('float32') / 255.0
-        train_labels = np.array(train_labels).astype('float32')
-        with open(FEATURES_TEST_OUT, 'wb') as f:
-            pickle.dump(train_features, f)
-
-        with open(LABELS_TEST_OUT, 'wb') as l:
-            pickle.dump(train_labels, l)
+#ImgSourceDirectory = './CASIA-WebFace/CASIA-WebFace'
+# https://drive.google.com/open?id=1Of_EVz-yHV7QVWQGihYfvtny9Ne8qXVz
+# The CASIA dataset is annotated with 10,575 unique people with 494,414 images in total.
 
 
-generateData(TRAIN_DIR)
-generateData(TEST_DIR, False)
+#ImgSourceDirectory = './self-built-masked-face-recognition-dataset/AFDB_face_dataset'
+# Default dataset
+
+
+PEOPLE = os.listdir(ImgSourceDirectory)
+
+images = []
+
+# Import images
+for index, person in enumerate(PEOPLE):
+    path = os.path.join(ImgSourceDirectory, person)
+    # print(path)
+    for image in os.listdir(path):
+        # Read in images as an array of pixel values
+        image_as_numpy_array = cv2.imread(
+            os.path.join(path, image), cv2.IMREAD_GRAYSCALE)
+        print(path, image)
+
+
+# The images must be shuffled, otherwise the network will learn incorrectly (it will learn to always guess the first guy, then the second guy, etc.)
+random.shuffle(images)
+
+features = []
+labels = []
+
+for f, l in images:
+    features.append(f)
+    labels.append(l)
+
+# Transform features into numpy array
+# reshape(amount_of_features, dimension, dimension, 1 = grayscale)
+features = np.array(features).reshape(-1, IMG_SIZE,
+                                      IMG_SIZE, 1).astype('float32') / 255.0
+labels = np.array(labels).astype('float64')
+
+# Save for later
+with open('features.pickle', 'wb') as f:
+    pickle.dump(features, f)
+
+
+with open('labels.pickle', 'wb') as l:
+    pickle.dump(labels, l)
--- trainModel.py
+++ trainModel.py
@@ -1,149 +1,142 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Activation, Dense, Flatten, BatchNormalization, Conv2D, MaxPool2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.metrics import categorical_crossentropy
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import os
+
 import pickle
 import numpy as np
-import tensorflow as tf
+import matplotlib.pyplot as plt
 import tensorflow_addons as tfa
-import os
-from tensorflow.keras.models import Model, Sequential, load_model
-from tensorflow.keras.layers import Dense, Conv2D, Input, MaxPooling2D, Flatten, Dropout, Lambda, BatchNormalization
-from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Input, MaxPooling2D, Flatten, Dropout, Lambda
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-TRAIN_PICKLE_LABELS = 'training_mask_labels.pickle'
-TRAIN_PICKLE_FEATURES = 'training_mask_features.pickle'
-TEST_PICKLE_LABELS = 'testing_mask_labels.pickle'
-TEST_PICKLE_FEATURES = 'testing_mask_features.pickle'
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+print("Num GPUs Available: ", len(physical_devices))
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
-VALIDATION_PICKLE_FEATURES = 'validation_mask_features.pickle'
-VALIDATION_PICKLE_LABELS = 'validation_mask_labels.pickle'
 
-IMG_SIZE = 64
-AMOUNT_CLASSES = 64
+IMG_SIZE = 64  # was 64
+AMOUNT_CLASSES = 460
 
 # Import data
-features_train = None
-labels_train = None
-features_test = None
-labels_test = None
+features = None
+labels = None
 
-features_validate = None
-labels_validate = None
+with open('features.pickle', 'rb') as f:
+    features = pickle.load(f)
 
-with open(TRAIN_PICKLE_FEATURES, 'rb') as f:
-    features_train = pickle.load(f)
-
-with open(TRAIN_PICKLE_LABELS, 'rb') as f:
-    labels_train = pickle.load(f)
-
-with open(TEST_PICKLE_LABELS, 'rb') as f:
-    labels_test = pickle.load(f)
-
-with open(TEST_PICKLE_FEATURES, 'rb') as f:
-    features_test = pickle.load(f)
-
-with open(VALIDATION_PICKLE_FEATURES, 'rb') as f:
-    features_validate = pickle.load(f)
-
-with open(VALIDATION_PICKLE_LABELS, 'rb') as f:
-    labels_validate = pickle.load(f)
+with open('labels.pickle', 'rb') as l:
+    labels = pickle.load(l)
 
 # Split into training and validation sets
-# features_train = features[:-2000]
-# features_test = features[-2000:]
-# labels_train = labels[:-2000]
-# labels_test = labels[-2000:]
+features_train = features[:-10000]
+features_test = features[-10000:]
+labels_train = labels[:-10000]
+labels_test = labels[-10000:]
 
+# added by BFW
+model = Sequential()
 
 # # Build model
 inputs = Input(shape=(IMG_SIZE, IMG_SIZE, 1))
+
+### 1 ###
 # Convolution
-x = Conv2D(32, (3, 3), input_shape=features_train.shape[1:],
+# input shape is one dimensional
+x = Conv2D(32, (3, 3), input_shape=features.shape[1:],
            activation="relu", use_bias=True, padding="same")(inputs)
 # Pooling
 x = MaxPooling2D(pool_size=(2, 2), padding="same")(x)
 # Dropout to reduce overfit
-# x = Dropout(0.1)(x)
+x = Dropout(0)(x)
+
+### 2 ###
 # Convolution
 x = Conv2D(64, (3, 3), activation="linear", use_bias=True, padding="same")(x)
 # Pooling
 x = MaxPooling2D(pool_size=(2, 2), padding="same")(x)
 # Dropout to reduce overfit
-x = Dropout(0.5)(x)
-# # Convolution
-# x = Conv2D(64, (3, 3), activation="linear", use_bias=True, padding="same")(x)
-# # Pooling
-# x = MaxPooling2D(pool_size=(2, 2), padding="same")(x)
-# # Dropout to reduce overfit
-# x = Dropout(0.1)(x)
-# # Convolution
-# x = Conv2D(128, (3, 3), activation="linear", use_bias=True, padding="same")(x)
-# # Pooling
-# x = MaxPooling2D(pool_size=(2, 2), padding="same")(x)
-# # Dropout to reduce overfit
-# x = Dropout(0.1)(x)
+x = Dropout(0.0)(x)
+
+### 3 ###
+# Convolution
+x = Conv2D(128, (3, 3), activation="linear", use_bias=True, padding="same")(x)
+# Pooling
+x = MaxPooling2D(pool_size=(2, 2), padding="same")(x)
+# Dropout to reduce overfit
+x = Dropout(0.3)(x)
+
+### 4 ###
+# Convolution
+x = Conv2D(256, (3, 3), activation="linear", use_bias=True, padding="same")(x)
+# Pooling
+x = MaxPooling2D(pool_size=(2, 2), padding="same")(x)
+# Dropout to reduce overfit
+x = Dropout(0.45)(x)
+
+### 5 ###
 # Convolutions
-x = Conv2D(128, (3, 3), activation="linear",
-           use_bias=True, padding="same")(x)
+x = Conv2D(512, (3, 3), activation="linear", use_bias=True, padding="same")(x)
 # Pooling
 x = MaxPooling2D(pool_size=(2, 2), padding="same")(x)
 # Flatten
 x = Flatten()(x)
+# Dropout to reduce overfit
+x = Dropout(0.65)(x)
+
+
+### 6 ###
 # Final
-x = Dense(128, activation="linear")(x)
+x = Dense(512, activation="linear")(x)
+
+### 7 ###
 # Output layer
 preds = Dense(AMOUNT_CLASSES, activation="softmax")(x)
 model = Model(inputs=inputs, outputs=preds)
 
-# The below model uses triplet loss, but doesn't work very well
 
-# model = tf.keras.Sequential([
-#   Conv2D(32, (3, 3), input_shape=features.shape[1:], activation="relu", use_bias=True, padding="same"),
-#   MaxPooling2D(pool_size=(2,2), padding="same"),
-#   Dropout(0.3),
-#   Conv2D(64, (3, 3), padding="same", activation="relu"),
-#   MaxPooling2D(pool_size=(2, 2)),
-#   Dropout(0.3),
-#   Conv2D(128, (3, 3), input_shape=features.shape[1:], activation="relu", use_bias=True, padding="same"),
-#   MaxPooling2D(pool_size=(2,2), padding="same"),
-#   Dropout(0.3),
-#   Flatten(),
-#   Dense(256, activation=None),
-#   Lambda(lambda x: tf.math.l2_normalize(x, axis=1))
-# ])
-
+# overview of the model components
 model.summary()
 
 # Train model
 model.compile(
     loss=tf.keras.losses.sparse_categorical_crossentropy,
+    # loss=tf.keras.losses.categorical_crossentropy,
+    # learning_rate=0.0009 added learning rate to lower loss
     optimizer=tf.keras.optimizers.Adam(),
     metrics=[
-        tf.keras.metrics.SparseCategoricalAccuracy(),
-        # 'accuracy'
+        # tf.keras.metrics.SparseCategoricalAccuracy(),
+        'accuracy'
     ]
 )
-# model.compile(
-#   loss=tfa.losses.TripletSemiHardLoss(),
-#   optimizer=tf.keras.optimizers.Adam(),
-#   metrics=['accuracy']
-# )
 
 
-print(len(features_test), len(features_train), len(features_validate))
-print(features_validate[0].shape)
-print(features_train.shape)
-print(labels_train.shape)
+history = model.fit(features_train, labels_train,
+                    batch_size=100, validation_split=0.3, epochs=40)
 
-model.fit(features_train, labels_train, batch_size=20, validation_split=0.2,
-          epochs=5)
+results = model.evaluate(features_test, labels_test, batch_size=100)
 
-
-model.save('./model')
-del model
-model = load_model('./model')
-# prediction = model.predict(features_validate, callbacks=[Callbacks()])
-
-
-results = model.evaluate(
-    features_test, labels_test, batch_size=20)
 print('test loss, test acc:', results)
+
+
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
